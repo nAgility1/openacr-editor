c6ee8d3b686ec4b953c6e3dce08b27d6670ea901
nAgility1 <nora786@gmail.com>
2024-12-19 17:01:41 -0500

    update Catalogs to leave WCAG 2.0 and add WCAG 2.2

--- src/utils/getCatalogs.js
+++ src/utils/getCatalogs.js
@@ -1,10 +1,12 @@
+// Update the imports to 2.5-edition-wcag-2.1-508-en.yaml once the catalog is updated in OpenACR.
+
 //import wcag20508catalog from "@openacr/openacr/catalog/2.4-edition-wcag-2.0-508-en.yaml";
-//import wcag21catalog from "@openacr/openacr/catalog/2.4-edition-wcag-2.1-en.yaml";
+import wcag21catalog from "@openacr/openacr/catalog/2.4-edition-wcag-2.1-en.yaml";
 //import wcag21508catalog from "@openacr/openacr/catalog/2.4-edition-wcag-2.1-508-en.yaml";
 import wcag21508catalog from "../data/2.5-edition-wcag-2.1-508-en.yaml";
 import wcag22508catalog from "../data/2.5-edition-wcag-2.1-508-en.yaml";
 //export const wcag20508catalogName = "2.4-edition-wcag-2.0-508-en";
-//const wcag21catalogName = "2.4-edition-wcag-2.1-en";
+const wcag21catalogName = "2.4-edition-wcag-2.1-en";
 //const wcag21508catalogName = "2.4-edition-wcag-2.1-508-en";
 const wcag21508catalogName = "2.5-edition-wcag-2.1-508-en";
 const wcag22508catalogName = "2.5-edition-wcag-2.2-508-en";
@@ -13,28 +15,34 @@ const wcag22508catalogName = "2.5-edition-wcag-2.2-508-en";
 export function getCatalog(catalogName) {
   if (catalogName == wcag21508catalogName) {
     return wcag21508catalog;
-  //} else if (catalogName == wcag21catalogName) {
-  //   return wcag21catalog;
+  } else if (catalogName == wcag21catalogName) {
+    return wcag21catalog;
+  } else if (catalogName == wcag21508catalogName) {
+    return wcag21508catalog;
   } else if (catalogName == wcag22508catalogName) {
     return wcag22508catalog;
   }
-  return wcag21508catalog;
+  return wcag20508catalog;
 }
 
 export function getListOfCatalogs() {
   return [
     {
+      catalog: wcag20508catalogName,
+      title: "VPAT® 2.5 508: Revised Section 508 Edition (WCAG 2.0)",
+    },
+    {
       catalog: wcag21508catalogName,
-      title: "VPAT® 2.5 508: Revised Section 508 Edition (WCAG 2.1)",
+      title: "VPAT® 2.5 508 + WCAG: Revised Section 508 Edition (WCAG 2.1)",
+    },
+    {
+      catalog: wcag21catalogName,
+      title: "VPAT® 2.4 WCAG: WCAG 2.1",
     },
     {
       catalog: wcag22508catalogName,
       title: "VPAT® 2.5 508 + WCAG: Revised Section 508 Edition (WCAG 2.2)",
-    }//,
-    // {
-    //   catalog: wcag21catalogName,
-    //   title: "VPAT® 2.4 WCAG: WCAG 2.1",
-    // },
+    },
   ];
 }
 
